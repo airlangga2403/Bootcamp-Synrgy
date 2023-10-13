540a0b67664b879563cdb49c5922ae735ac882b6
Zeeus <airlanggapermana96@gmail.com>
2023-10-13 16:52:04 +0700

    Implement Testing UserRepository

--- challange3/src/test/java/com/example/synrgy6/repository/UsersRepositoryTest.java
+++ challange3/src/test/java/com/example/synrgy6/repository/UsersRepositoryTest.java
@@ -1,13 +1,29 @@
 package com.example.synrgy6.repository;
 
+import com.example.synrgy6.model.Users;
+import com.example.synrgy6.utils.Utils;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-
+@Rollback(value = false)
 class UsersRepositoryTest {
 
+    @Autowired private UsersRepository usersRepository;
+
+    @Test
+    public void testCreateUser() {
+        String randomString = Utils.generateRandomString();
+        Users users = new Users("airlanggaxxx" + randomString, "airlanggaemailamantap@gmail.com","12345");
+        Users savedUser = usersRepository.save(users);
+        assertThat(savedUser).isNotNull();
+    }
 }
